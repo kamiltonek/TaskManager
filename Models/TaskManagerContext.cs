05ac1e6bc9a8db09f38801999cb9446329b458b6
kamil <kamiltonek@wp.pl>
2021-02-18 15:15:34 +0100

    Init

---
+++ Models/TaskManagerContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Models;
+
+namespace TaskManager.Repositiories
+{
+    public class TaskManagerContext : DbContext
+    {
+        public TaskManagerContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<TaskModel> Tasks { get; set; }
+    }
+}
---
+++ Repositiories/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Models;
+
+namespace TaskManager.Repositiories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int taskID);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskId, TaskModel task);
+        void Delete(int taskId);
+    }
+}
